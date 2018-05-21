9c60839c31afd6985ea577d244894f2a6b9f3de2
wxgaly <w962831870@gmail.com>
2018-05-22 01:09:19 +0800

    增加mybatis相关代码

---
+++ src/main/java/wxgaly/example/springboot/mapper/SysUserMapper.java
@@ -0,0 +1,7 @@
+package wxgaly.example.springboot.mapper;
+
+import wxgaly.example.springboot.pojo.SysUser;
+import wxgaly.example.springboot.utils.MyMapper;
+
+public interface SysUserMapper extends MyMapper<SysUser> {
+}
