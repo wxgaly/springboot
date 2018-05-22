68b401719b8131e9b98838a4005b5a38147e4076
wxgaly <w962831870@gmail.com>
2018-05-23 00:49:39 +0800

    增加mybatis相关代码

--- src/main/java/wxgaly/example/springboot/SpringbootApplication.java
+++ src/main/java/wxgaly/example/springboot/SpringbootApplication.java
@@ -1,12 +1,18 @@
 package wxgaly.example.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//扫描 mybatis mapper 包路径
+@MapperScan(basePackages = "wxgaly.example.springboot.mapper")
+//扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
+@ComponentScan(basePackages = {"wxgaly.example.springboot", "org.n3r.idworker"})
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
 }
